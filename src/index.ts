4c92e68c68ad6842baa71b996d745aa84b970d80
infinite1400 <muraripandey1886@gmail.com>
2024-04-19 16:55:12 +0530

    project set-up

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app=express();
+
+app.use(cors({
+    credentials : true
+}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+
+const server=http.createServer(app);
+const PORT=process.env.PORT || 8080;
+server.listen(PORT,()=>{
+    console.log(`Server is running on http/:localhost:${PORT}`)
+})
